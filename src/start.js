096415a00615bf12de90a95d6f38db3f29d8d5c8
Ziggy Jonsson <ziggy.jonsson.nyc@gmail.com>
2013-02-05 20:14:53 -0500

    Clarify javascript version

--- src/start.js
+++ src/start.js
@@ -1,5 +1,5 @@
 /**
-  * pyfy (c) 2008-2013 Sigurgeir Orn Jonsson (ziggy.jonsson.nyc@gmail.com)
+  * pyfy.js (c) 2008-2013 Sigurgeir Orn Jonsson (ziggy.jonsson.nyc@gmail.com)
   * @license http://creativecommons.org/licenses/by-nc-sa/3.0/
   * Creative Commons Attribution-NonCommercial-ShareAlike 3.0 Unported (CC BY-NC-SA 3.0)
   */
