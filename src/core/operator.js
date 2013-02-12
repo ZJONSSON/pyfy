3dfcbd77936d09534fa7beca096308ad0d577674
Ziggy Jonsson <ziggy.jonsson.nyc@gmail.com>
2013-02-12 18:47:21 -0500

    Add Math.pow as an operator

--- src/core/operator.js
+++ src/core/operator.js
@@ -5,6 +5,7 @@ var ops = {
   sub:function(a,b) { return a-b; },
   mul:function(a,b) { return a*b; },
   div:function(a,b) { return a/b; },
+  pow:function(a,b) { return Math.pow(a,b); }
 };
 
 Object.keys(ops).forEach(function(op) {
