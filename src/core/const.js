02df7f92b20782aad85c4eabaae1687888559e29
Ziggy Jonsson <ziggy.jonsson.nyc@gmail.com>
2013-02-12 18:47:05 -0500

    Allow update of Const

--- src/core/const.js
+++ src/core/const.js
@@ -11,3 +11,8 @@ Const.prototype = new Base();
 Const.prototype.fn = function() {
   return this.const;
 };
+
+Const.prototype.update = Const.prototype.set = function(d) {
+  this.const = d;
+  return this;
+};
