b7e8b524e799298c9b669150b1f13d74603136b7
Ziggy Jonsson <ziggy.jonsson.nyc@gmail.com>
2013-03-13 20:21:54 -0400

    Allow for initial value in Last By default this is zero

--- src/core/deriv/last.js
+++ src/core/deriv/last.js
@@ -2,8 +2,9 @@
 
 pyfy.Last = Last;
 
-function Last(d) {
+function Last(d,start) {
   Derived.call(this,d);
+  this.default = start || 0;
 }
 
 Last.prototype = new Derived();
@@ -11,6 +12,5 @@ Last.prototype = new Derived();
 Last.prototype.fn = function(res,d) {
   var dates = this.dates(res),
       datePos = res.cache[this.ID].datePos[d];
-
-  return (datePos > 0) ? res.fetch(this.parent,dates[datePos-1]) : 0;  
+  return (datePos > 0) ? res.fetch(this.parent,dates[datePos-1]) : this.default;  
 };
