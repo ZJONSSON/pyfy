42821ccf0d670414f8740be589db9746c53f09e9
Ziggy Jonsson <ziggy.jonsson.nyc@gmail.com>
2013-03-20 18:59:17 -0400

    Object versioning Dynamically figure out which parts of the cache need to be updated. Allow object rawDates to filter the rawDates coming from parents (i.e. period)

--- src/core/deriv/period.js
+++ src/core/deriv/period.js
@@ -3,7 +3,7 @@
 pyfy.period = Period;
 
 function Period(d,start,fin) {
-	if (!(this instanceof Period))
+  if (!(this instanceof Period))
     return new Period(d,start,fin);
   Derived.call(this,d);
   this.args.start = start || -Infinity;
@@ -11,6 +11,16 @@ function Period(d,start,fin) {
 }
 
 Period.prototype = new Derived();
+
+Period.prototype.rawDates = function(rawDates) {
+  var res = {};
+  if (rawDates)
+    Object.keys(rawDates).forEach(function(key) {
+      var d = rawDates[key];
+      if (d>= this.args.start && d <= this.args.fin) res[d] = d;
+    },this);
+  return res;
+};
 
 Period.prototype.fn = function(query,d) {
   return (d >= this.args.start && d<= this.args.fin) ? query.fetch(this.args.parent,d) : 0;
--- src/core/query.js
+++ src/core/query.js
@@ -17,22 +17,34 @@ function Query() {
   this.cache = {};
 }
 
-Query.prototype.getCache = function(obj) {
-  var cache = this.cache;
-  if (!cache[obj.ID]) {
-    this.cache[obj.ID] = {values:{}, children:[]};
-    if (obj.args) Object.keys(obj.args).forEach(function(key) {
-      var parent = obj.args[key];
-      var pc = this.getCache(parent);
-      if (pc) pc.children.push(this);
+// Init cache traverses the cache object upwards
+// 
+
+Query.prototype.initCache = function(obj) {
+  if (!obj.ID) return false;
+
+  var clear = (!this.cache[obj.ID] || this.cache[obj.ID].version != obj.version);
+
+  if (obj.args) Object.keys(obj.args).forEach(function(key) {
+    var parent = obj.args[key];
+    if (this.initCache(parent)) clear = true;
     },this);
+
+  if (clear) {
+    this.cache[obj.ID] = {values:{},version:obj.version};
   }
-  return cache[obj.ID];
+
+  return clear;
 };
 
+Query.prototype.getCache = function(obj) {
+  this.initCache(obj);
+  return this.cache[obj.ID];
+};
 
 Query.prototype.dates = function(obj) {
   var cache = this.getCache(obj);
+  if (!cache) return [];
   if (!cache.dates) {
     var rawDates = this.rawDates(obj),
         dates = cache.dates = [];
@@ -50,12 +62,13 @@ Query.prototype.rawDates = function(obj,rawDates) {
   var cache = this.getCache(obj);
   rawDates = rawDates || {};
 
-  if (!cache.rawDates) {
-    if (obj.rawDates) obj.rawDates(rawDates);
+  if (cache && !cache.rawDates) {
     if (obj.args) Object.keys(obj.args).forEach(function(key) {
-      this.rawDates(obj.args[key],rawDates);
+      rawDates = this.rawDates(obj.args[key],rawDates);
     },this);
-    //cache.rawDates = rawDates;
+    // Finally we check to see if current object has inherit rawDates to contribute
+    // This also gives current object the opportunity to overwrite rawDates so far (i.e. filter)
+    if (obj.rawDates) rawDates = obj.rawDates(rawDates);
   }
   return rawDates;
 };
@@ -85,7 +98,7 @@ Query.prototype.get = function(obj,d) {
 
 Query.prototype.fetch = function(obj,d) {
   if (!isNaN(obj)) return obj;  // in case it's a number
-  var values = this.getCache(obj).values; 
+  var values = this.getCache(obj).values;
 
   if (values[d] === undefined) {
     var fn = obj.fn(this,d.valueOf());
